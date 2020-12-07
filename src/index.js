89e24758e89d7905d04d71ee85428bd711d1c500
volkanb <volkanbenli92@gmail.com>
2020-12-07 18:03:38 -0500

    Duplicates comment JSX to create new comments

--- src/index.js
+++ src/index.js
@@ -7,7 +7,35 @@ const App = () => {
 		<div className="ui container comments">
 			<div className="comment">
 				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.people()} />
+					<img alt="avatar" src={faker.image.image()} />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM</span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.image()} />
+				</a>
+				<div className="content">
+					<a href="/" className="author">
+						Sam
+					</a>
+					<div className="metadata">
+						<span className="date">Today at 6:00PM</span>
+					</div>
+					<div className="text">Nice blog post!</div>
+				</div>
+			</div>
+			<div className="comment">
+				<a href="/" className="avatar">
+					<img alt="avatar" src={faker.image.image()} />
 				</a>
 				<div className="content">
 					<a href="/" className="author">
