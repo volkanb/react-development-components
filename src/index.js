7c39a375f1938c1b3106c2cc8850082559427ab5
volkanb <volkanbenli92@gmail.com>
2020-12-07 18:54:41 -0500

    Replace JSX with CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,54 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import faker from "faker";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
 
 const App = () => {
 	return (
-		<div className="ui container comments">
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.image()} />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blog post!</div>
-				</div>
-			</div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.image()} />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blog post!</div>
-				</div>
-			</div>
-			<div className="comment">
-				<a href="/" className="avatar">
-					<img alt="avatar" src={faker.image.image()} />
-				</a>
-				<div className="content">
-					<a href="/" className="author">
-						Sam
-					</a>
-					<div className="metadata">
-						<span className="date">Today at 6:00PM</span>
-					</div>
-					<div className="text">Nice blog post!</div>
-				</div>
-			</div>
+		<div className='ui container comments'>
+			<CommentDetail />
+			<CommentDetail />
+			<CommentDetail />
+			<CommentDetail />
+			<CommentDetail />
 		</div>
 	);
 };
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(<App />, document.querySelector('#root'));
