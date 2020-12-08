ce8e23de0457503ed2a5f7372b63340d6f2663b4
volkanb <volkanbenli92@gmail.com>
2020-12-07 19:29:00 -0500

    Adds more props to CommentDetail component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
 	return (
 		<div className='ui container comments'>
-			<CommentDetail author='Sam' />
-			<CommentDetail author='Alex' />
-			<CommentDetail author='Jane' />
+			<CommentDetail
+				author='Sam'
+				timeAgo='Today at 4:45PM'
+				commentText='Nice blog post!'
+				imgSrc={faker.image.image()}
+			/>
+			<CommentDetail
+				author='Alex'
+				timeAgo='Today at 2:00AM'
+				commentText='Not a good post!'
+				imgSrc={faker.image.image()}
+			/>
+			<CommentDetail
+				author='Jane'
+				timeAgo='Yesterday at 5:00PM'
+				commentText='Props are great!'
+				imgSrc={faker.image.image()}
+			/>
 		</div>
 	);
 };
