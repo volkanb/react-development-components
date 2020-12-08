3a3b81b3066de236d0c8c359b94a3ac0cc399258
volkanb <volkanbenli92@gmail.com>
2020-12-07 19:08:37 -0500

    Adds author property to CommentDetail components

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
-import React from "react";
-import faker from "faker";
+import React from 'react';
+import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
 	return (
-		<div className="comment">
-			<a href="/" className="avatar">
-				<img alt="avatar" src={faker.image.image()} />
+		<div className='comment'>
+			<a href='/' className='avatar'>
+				<img alt='avatar' src={faker.image.image()} />
 			</a>
-			<div className="content">
-				<a href="/" className="author">
-					Sam
+			<div className='content'>
+				<a href='/' className='author'>
+					{props.author}
 				</a>
-				<div className="metadata">
-					<span className="date">Today at 6:00PM</span>
+				<div className='metadata'>
+					<span className='date'>Today at 6:00PM</span>
 				</div>
-				<div className="text">Nice blog post!</div>
+				<div className='text'>Nice blog post!</div>
 			</div>
 		</div>
 	);
